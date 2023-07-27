4d9a0658de64583a5fd61978a848e34a9cf2bfd9
Rimberse <kerimpltm@gmail.com>
2023-07-27 12:47:02 +0200

    Implement Event and Segment comparators

--- src/main/kotlin/controller/Algorithm.kt
+++ src/main/kotlin/controller/Algorithm.kt
@@ -14,8 +14,35 @@ class Algorithm(var inputData: List<Segment>) {
     private val coordinates: List<Point>
 
     init {
-        queue = PriorityQueue();
-        tree = TreeSet();
+        queue = PriorityQueue(eventComparator());
+        tree = TreeSet(segmentComparator());
         coordinates = ArrayList();
+
+        for (segment: Segment in inputData) {
+            queue.add(Event(segment.start, segment, 0));
+            queue.add(Event(segment.end, segment, 1));
+        }
+    }
+
+    private inner class eventComparator : Comparator<Event> {
+        override fun compare(e1: Event, e2: Event): Int {
+            if (e1.value > e2.value)
+                return 1;
+            else if (e1.value < e2.value)
+                return -1;
+
+            return 0;
+        }
+    }
+
+    private inner class segmentComparator : Comparator<Segment> {
+        override fun compare(s1: Segment, s2: Segment): Int {
+            if (s1.value < s2.value)
+                return 1;
+            else if (s1.value > s2.value)
+                return -1;
+
+            return 0;
+        }
     }
 }
